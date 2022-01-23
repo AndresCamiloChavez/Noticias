04fb8fe4533efac90d3fc6c35d33ea838ed654f9
Andres Chavez <andres.cz.chavez@gmail.com>
2022-01-23 13:10:13 -0500

    Manejo de noticias por componentes

--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RespuestaTopHeadlines } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,8 @@ export class NoticiasService {
   constructor(private http: HttpClient) { }
 
   getTopHeadlines(){
-    return this.http.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=6a16267384034297b4dadccdbd539666`);
+    return this.http.get<RespuestaTopHeadlines>(`
+    https://newsapi.org/v2/top-headlines?country=co&category=business&apiKey=6a16267384034297b4dadccdbd539666`);
   }
 
 }
